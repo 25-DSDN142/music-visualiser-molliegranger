4623b4fd0d24d8192af49a23bf8ae5b7667342b2
molliegranger <Molliemaygranger@gmail.com>
2025-09-09 20:39:27 +1200

    Update music_starter.js

--- music_starter.js
+++ music_starter.js
@@ -1,7 +1,7 @@
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(20)
+  background(236, 222, 255)
   textFont('Verdana'); // please use CSS safe fonts
   rectMode(CENTER)
   textSize(24);
