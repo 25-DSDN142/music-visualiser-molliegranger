04f71d1261459e104c9f3edaab3aaae05ea5351f
molliegranger <Molliemaygranger@gmail.com>
2025-09-11 22:22:02 +1200

    updates

--- music_starter.js
+++ music_starter.js
@@ -1,26 +1,36 @@
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 
-let myImage;
+let laceImg;
+let oliviaImg;
+let mirrorImg;
+
 let firstRun = true;
+let laceX = 0;
+let bottomlaceX = 0;
 
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
    if (firstRun) {
-  myImage = loadImage("lace-border.png");
+  laceImg = loadImage("lace-border.png");
+  oliviaImg = loadImage("olivia.png")
+  mirrorImg = loadImage("mirror.png")
   firstRun = false;
 }
   background(236, 222, 255)
   //top lace
-  if (myImage) {
-   let offset = map (counter,0,100, -50, 50);
-  
-   image(myImage, offset, 30, width, 40); 
- 
+  if (laceImg) {
+   laceX = (laceX + 2) % width;
+  bottomlaceX = (bottomlaceX - 2 + width) % width;
+
+  image(laceImg, laceX, 30, width, 40); 
+  image(laceImg, laceX - width, 30, width, 40);
+
    //bottom lace
   push ();
   translate (0, height);
   scale(1,-1);
-  image(myImage, -offset, 30, width, 40); 
+  image(laceImg, bottomlaceX, 30, width, 40);
+  image(laceImg, bottomlaceX - width, 30, width, 40);
   pop ();
   }
   textFont('Verdana'); // please use CSS safe fonts
@@ -62,4 +72,10 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
    textAlign(CENTER);
    textSize(vocal);
    text(words, width/2, height/3);
+   if (oliviaImg && mirrorImg){
+   let bob = map (drum, 0,100, -10,10);
+   image (oliviaImg, width/2 - 100, height/2 - 150 + bob, 200, 300);
+   image (mirrorImg, width/2 - 120, height/2 - 180 + bob, 240, 360);
+
+   }
 }
